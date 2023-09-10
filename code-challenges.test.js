eecafc3f1492c41dc94c4672b41d7c409cd1bf57
Aleja Hale <alejajara2@gmail.com>
2023-09-10 14:10:17 -0700

    assesment done, I added yarn and package.json to be able to check the tests

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -34,9 +34,67 @@ const object2 = { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
-// b) Create the function that makes the test pass.
+describe ("evenDivisible", () => {
+    it("takes a object as an argument and decides if the number inside it is evenly divisible by three or not.", () => {
+        expect(evenDivisible(15)).toEqual("15 is divisible by three")
+        expect(evenDivisible(0)).toEqual("0 is divisible by three")
+        expect(evenDivisible(-7)).toEqual("-7 is not divisible by three")
 
+    })
+}) // Failed succesfully: output: ReferenceError: evenDivisible is not defined
+
+// b) Create the function that makes the test pass.
+// -------------- FIRST ATTEMPT AND IT DIDNT WORK: THE TEST CONTINUES TO SAY THAT evenDivisible is not defined---------------
 // Pseudo code:
+// name: evenDivisible
+// input: an object as an argument
+// output: a string that says if the number is even divisible by three or not
+// process: add a function to the object, and use conditionals to see if the number is evenly divisble by 3
+// const object1 = { 
+//     number: 15, 
+//     evenDivisible: function () {
+//         if (this.number % 3 === 0) {
+//             return `${this.number} is divisible by three`
+//         } else {
+//             return `the number is not even divisible`
+//         }
+//     }
+//     }
+// const object2 = { 
+//     number: 0, 
+//     evenDivisible: function () {
+//         if (this.number % 3 === 0) {
+//             return `${this.number} is divisible by three`
+//         } else {
+//             return `the number is not even divisible`
+//         }
+//     }
+//     }
+// const object3 = { 
+//     number: -7, 
+//     evenDivisible: function () {
+//         if (this.number % 3 === 0) {
+//             return `${this.number} is divisible by three`
+//         } else {
+//             return `the number is not even divisible`
+//         }
+//     }
+//     }
+// ------------------------ THIS DIDNT WORK----------------------------------
+// -------------------- SECOND ATTEMPT : SUCCESSFULL ATTEMPT --------------------------------------
+// Pseudo code:
+// name: evenDivisible
+// input: an object as an argument
+// output: a string that says if the number is even divisible by three or not
+// process: create a new function, and use conditionals to see if the number is evenly divisble by 3
+const evenDivisible = (number) => {
+    if (number % 3 === 0) {
+        return `${number} is divisible by three`
+    } else {
+        return `${number} is not divisible by three`
+    }
+}
+// ---------------------- TEST PASSED --------------------------------
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
@@ -47,13 +105,38 @@ const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
+describe ("allCaps", () => {
+    it("takes an array of words and returns an array with all the words capitalized", () => {
+        expect(allCaps(["streetlamp", "potato", "teeth", "conclusion", "nephew"])).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(allCaps(["temperature", "database", "chopsticks", "mango"])).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+    })
+})
+// Failed succesfully: output: ReferenceError: allCaps is not defined
+
 // b) Create the function that makes the test pass.
-
 // Pseudo code:
-
+// name: allCaps
+// input: an array with words in lowercase
+// output: an array with all the words capitalized
+// process: a function with a for loop, I'll create a new array to store the new array, and use .toUpperCase() to capitalize the first word and then slice()to add the rest of the array starting at index 1
+const allCaps = (array) => {
+    let capitalizedWords = []
+ for (let i = 0; i < array.length; i++) {
+   capitalizedWords.push(array[i][0].toUpperCase() + array[i].slice(1))
+ } return capitalizedWords
+}
+console.log(allCaps(randomNouns1))
+// ---------------------- TEST PASSED ------------------------
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
-
 // a) Create a test with expect statements for each of the variables provided.
+describe ("indexes", () => {
+    it("takes in an string and logs the index of the first vowel", () => {
+        expect(indexes("learn")).toEqual(1)
+        expect(indexes("academy")).toEqual(0)
+        expect(indexes("challenges")).toEqual(2)
+    })
+})
+// Failed succesfully: output: ReferenceError: indexes is not defined
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -63,5 +146,17 @@ const vowelTester3 = "challenges"
 // Expected output: 2
 
 // b) Create the function that makes the test pass.
-
 // Pseudo code:
+// name: indexes
+// input: a string 
+// output: logs the index of the first vowel
+// process: create a function with a loop that goes through the string and inside of it a conditional statement that looks if the string index contains a vowel 
+const indexes = (string) => {
+    for (let i = 0; i < string.length; i++) {
+        if ("AEIOUaeiou".includes(string[i])){
+            return(i)
+        }   
+    }
+} 
+console.log(indexes(vowelTester3))
+// ---------------------- TEST PASSED ------------------------
